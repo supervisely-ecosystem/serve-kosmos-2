c6dec8d66e29b750f4556fb415be42951352abc2
vorozhkog <vorozhkog@icloud.com>
2026-05-13 19:22:21 +0100

    init

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/kosmos-2:1.0.2 . && \
-docker push supervisely/kosmos-2:1.0.2
+docker build -t supervisely/kosmos-2:6.73.567 . && \
+docker push supervisely/kosmos-2:6.73.567
